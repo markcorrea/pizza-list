db65a6f00275711d37b2e9424fa818168ab53e67
Marcus Correa <mrc.correa@gmail.com>
2019-02-23 23:50:16 -0300

    Started separating components

--- src/components/Pizza/PizzaBoard.js
+++ src/components/Pizza/PizzaBoard.js
@@ -1,24 +1,14 @@
 import React from 'react'
 import { connect } from 'react-redux'
 
-import { defaultCheckboxValues } from './utilities'
 import { checkTopping, setSizeFirstState } from '../../actions/sizeAction'
 import { addToCart, removeFromCart } from '../../actions/cartAction'
+import SizeContainer from './SizeContainer'
 
 class PizzaBoard extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {}
-  }
 
   componentDidMount() {
     this.props.setSizeFirstState(this.props.data)
-  }
-
-  countToppings = toppings => {
-    return toppings.reduce((accumulator, item) => {
-      return item.checked ? accumulator + 1 : accumulator
-    }, 0)
   }
 
   sumSizePrice = (toppings, basePrice) => {
@@ -40,15 +30,6 @@ class PizzaBoard extends React.Component {
         .toFixed(2)
     )
   }
-
-  // deleteSize = index => {
-  //   const { cart } = this.state
-
-  //   cart.splice(index, 1)
-  //   this.setState({
-  //     cart: cart,
-  //   })
-  // }
 
   render() {
     return (
@@ -76,70 +57,8 @@ class PizzaBoard extends React.Component {
         <div className='card-container'>
           <div className='row'>
             {pizzaSizes.map(
-              ({ name, basePrice, toppings, maxToppings }, sizeIndex) => (
-                <div key={`size_board_${sizeIndex}`} className='col-md-4'>
-                  <div className='card'>
-                    <div className='pizza-card-header'>
-                      <span className='card-pizza-size'>{name}</span>
-                      <div className='card-pizza-price'>${basePrice}</div>
-                    </div>
-                    <span className='card-topping'>Toppings:</span>
-                    <ul className='option-list'>
-                      {toppings.map(
-                        (
-                          { topping: { name: toppingName, price }, checked },
-                          toppingIndex
-                        ) => (
-                          <li
-                            key={`topping_${sizeIndex}${toppingIndex}`}
-                            className='card-info'
-                          >
-                            <input
-                              type='checkbox'
-                              onChange={() =>
-                                this.props.checkTopping({
-                                  name: `${sizeIndex}.${toppingIndex}.checked`,
-                                  checked: checked,
-                                })
-                              }
-                              checked={checked}
-                              disabled={
-                                checked === false &&
-                                maxToppings !== null &&
-                                this.countToppings(toppings) >= maxToppings
-                              }
-                            />{' '}
-                            {toppingName}
-                            <span className='topping-price'>{price}</span>
-                          </li>
-                        )
-                      )}
-                    </ul>
-
-                    <div className='size-total-value'>
-                      Total:
-                      <span>${this.sumSizePrice(toppings, basePrice)}</span>
-                    </div>
-
-                    <div className='topping-select-counter'>
-                      You can select:{' '}
-                      <span>
-                        {this.countToppings(toppings)}/{maxToppings || '-'}
-                      </span>
-                    </div>
-
-                    <div className='pizza-card-footer mt-10'>
-                      <button
-                        onClick={() =>
-                          this.props.addToCart(pizzaSizes[sizeIndex])
-                        }
-                        className='btn-add-to-cart'
-                      >
-                        Add to cart
-                      </button>
-                    </div>
-                  </div>
-                </div>
+              (size, sizeIndex) => (
+                <SizeContainer key={`size_board_${sizeIndex}`} size={size} sizeIndex={sizeIndex}></SizeContainer>
               )
             )}
           </div>
--- src/reducers/sizeReducer.js
+++ src/reducers/sizeReducer.js
@@ -1,13 +1,13 @@
-const sizeReducer = (state = {}, action) => {
+const sizeReducer = (
+  state = {},
+  action
+) => {
   switch (action.type) {
     case 'CHECK_TOPPING':
       const keySplit = action.payload.name.split('.')
       state.pizzaSizes[keySplit[0]].toppings[keySplit[1]].checked = !action
         .payload.checked
-      state = {
-        ...state,
-        data: state,
-      }
+      state = Object.assign({}, state)
       break
     case 'SET_SIZE_FIRST_STATE':
       state = Object.assign({}, action.payload)
